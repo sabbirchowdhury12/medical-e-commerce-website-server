a107903aa5cda71d1d867d5cf584642d55fe38b9
Sabbir <sabbirchowdhury40854@gmail.com>
2025-04-27 23:27:55 +0600

    add origin

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'https://pharmaplus-sabbirchowdhury12.vercel.app/',
+      'https://pharmaplus-sabbirchowdhury12.vercel.app',
     ],
     credentials: true,
   }),
